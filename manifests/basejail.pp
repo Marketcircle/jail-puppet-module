8ad1d04c3f895521b5143c870195b80bbfc6d904
Thomas Bartelmess <tbartelmess@marketcircle.com>
2014-10-10 02:08:24 -0400

    Use script to install basejail

--- manifests/basejail.pp
+++ manifests/basejail.pp
@@ -5,61 +5,17 @@ class jail::basejail (
     $freebsd_download_path = $jail::params::freebsd_download_path
 ) inherits jail::params {
 
-    # Make sure the basejail location exists
-    # For now we just do folder, ZFS will be added later
+  $basejail_script = "${jail::scripts_dir}/build_basejail.sh"
+  file {$basejail_script:
+    ensure => present,
+    mode   => 744,
+    owner  => 'root',
+    group  => 'wheel',
+    source => 'puppet:///modules/jail/build_basejail.sh'
+  }
 
-    file {$basejail_location:
-        ensure  => directory,
-        recurse => true
-    }
-    file {$freebsd_download_path:
-        ensure  => directory,
-        recurse => true
-    }
-
-    $base_download_path = "${freebsd_download_path}/base.txz"
-    $base_download_url = "${freebsd_download_url}/base.txz"
-    exec {'base.txz':
-        creates => $base_download_path,
-        command => "ftp -o ${base_download_path} ${base_download_url}",
-        path    => "/usr/bin",
-        require => [File[$freebsd_download_path]],
-    }
-
-    $doc_download_path = "${freebsd_download_path}/doc.txz"
-    $doc_download_url = "${freebsd_download_url}/doc.txz"
-    exec {'doc.txz':
-        creates => $doc_download_path,
-        command => "ftp -o ${doc_download_path} ${doc_download_url}",
-        path    => "/usr/bin",
-        require => [File[$freebsd_download_path]],
-    }
-
-    $games_download_path = "${freebsd_download_path}/games.txz"
-    $games_download_url = "${freebsd_download_url}/games.txz"
-    exec {'games.txz':
-        creates => $games_download_path,
-        command => "ftp -o ${games_download_path} ${games_download_url}",
-        path    => "/usr/bin",
-        require => [File[$freebsd_download_path]],
-    }
-
-    $lib32_download_path = "${freebsd_download_path}/lib32.txz"
-    $lib32_download_url = "${freebsd_download_url}/lib32.txz"
-    exec {'lib32.txz':
-        creates => $lib32_download_path,
-        command => "ftp -o ${lib32_download_path} ${lib32_download_url}",
-        path    => "/usr/bin",
-        require => [File[$freebsd_download_path]],
-    }
-
-    $ports_download_path = "${freebsd_download_path}/ports.txz"
-    $ports_download_url = "${freebsd_download_url}/ports.txz"
-    exec {'ports.txz':
-        creates => $ports_download_path,
-        command => "ftp -o ${ports_download_path} ${ports_download_url}",
-        path    => "/usr/bin",
-        require => [File[$freebsd_download_path]],
-    }
-
-}   
+  exec {'build basejail':
+    command => "${basejail_script} -d ${freebsd_download_path} -b ${basejail_location}",
+    creates => $basejail_location
+  }
+}
