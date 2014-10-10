ad7cbfe68b1b56eccbad6cc4f252b51c5a6ffa29
Thomas Bartelmess <tbartelmess@marketcircle.com>
2014-10-10 02:08:10 -0400

    Added scripts to create jails and basejails

---
+++ files/build_jail.sh
@@ -0,0 +1,84 @@
+#! /usr/bin/env bash
+
+usage() {
+cat << EOF
+Usage $0 [OPTIONS...]
+
+This script creates a jail based on a basejail
+
+Options:
+  -b basejail
+  -f force (delete jail if exists)
+  -l location
+EOF
+}
+
+BASEJAIL=
+LOCATION=
+FORCE=0
+
+while getopts "hfb:l:" OPTION
+do
+  case $OPTION in
+    h)
+      usage
+      exit 0
+      ;;
+    f)
+      FORCE=1
+      ;;
+    l)
+      LOCATION=$OPTARG
+      ;;
+    b)
+      BASEJAIL=$OPTARG
+      ;;
+  esac
+done
+
+# Check that all required parameters are set
+
+if [ -z $BASEJAIL ]; then
+  echo "No basejail path provided"
+  exit 1
+fi
+
+if [ -z $LOCATION ]; then
+  echo "No jail location provided"
+  exit 1
+fi
+
+if [ -e $LOCATION ]; then
+  if [[ $FORCE == 1 ]]; then
+    echo "Jail location already exists, deleting"
+    rm -rf $LOCATION
+    if [[ $? != 0 ]]; then
+      echo "Failed to delete old jail at ${LOCATION}"
+      exit 1
+    fi
+  else
+    echo "Jail location already exists, exiting."
+    exit 1
+  fi
+fi
+
+mkdir -p $LOCATION
+MK_FOLDERS="usr"
+SYMLINK_FOLDERS="bin boot lib libexec rescue sbin sys usr/bin usr/include usr/lib usr/lib32 usr/libdata usr/libexec usr/ports usr/sbin usr/share usr/src"
+COPY_FOLDERS="etc root mnt tmp var usr/local usr/games usr/obj usr/tests"
+
+mkdir -p "${LOCATION}/basejail"
+
+for folder in $MK_FOLDERS; do
+  mkdir -p "${LOCATION}/${folder}"
+done
+
+for folder in $COPY_FOLDERS; do
+  cp -r "${BASEJAIL}/${folder}" "${LOCATION}/${folder}"
+done
+
+for folder in $SYMLINK_FOLDERS; do
+  echo $folder
+  ln -s "/basejail/${folder}" "${LOCATION}/${folder}" 
+done
+
