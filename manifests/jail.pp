26a42bd0a3489d80a551cab74a4f43f5db5cd14b
Thomas Bartelmess <tbartelmess@marketcircle.com>
2014-10-20 00:00:32 -0400

    Proper ensure support

--- manifests/jail.pp
+++ manifests/jail.pp
@@ -7,49 +7,20 @@
 # [*hostname*]
 #   Hostname of the jail
 #
-# [*ipv4_address*]
-#
-# [*ipv6_address*]
-#
-# [*interface*]
-#
-# [*mount_devfs*]
-#
-# [*allow_set_hostname*]
-#
-# [*allow_sysvipc*]
-#
-# [*allow_raw_sockets*]
-#
-# [*allow_chflags*]
-#
-# [*allow_mount*]
-#
-# [*allow_mount_nullfs*]
-#
-# [*allow_mount_procfs*]
-#
-# [*allow_mount_tmpfs*]
-#
-# [*allow_mount_zfs*]
-#
-# [*allow_quotas*]
-#
-# [*allow_socket_af*]
-#
-# [*ip4_addr*]
-#
-# [*ip6_addr*]
-
+# [* vnet_enable *]
+# Create the	prison with its	own virtual network stack, with	its
+# own network interfaces, addresses,	routing	table, etc.
 define jail::jail(
-  $hostname,
-  $ipv4_address,
-  $ipv6_address,
-  $interface,
+  $ensure = running,
+  $hostname = $name,
   $vnet_enable = false,
   $vnet_mode = 'new',
   $vnet_interfaces = undef,
   $vnet_interfaces_bridge = undef,
+  $ip4 = 'inherit',
+  $ip4_addr = undef,
+  $ip6 = 'inherit',
+  $ip6_addr = undef,
   $mount_devfs = true,
   $allow_set_hostname = false,
   $allow_sysvipc = false,
@@ -63,8 +34,6 @@ define jail::jail(
   $allow_mount_zfs = false,
   $allow_quotas = false,
   $allow_socket_af = false,
-  
-
   $install_puppet = true
 ){
   $jail_location = "${jail::jails_location}/${name}"
@@ -73,18 +42,37 @@ define jail::jail(
 
   $manage_file_path = "${jail::config_dir}/${name}.conf"
 
-  exec {"create-${name}-jail":
-    command => "${jail::create_jail_script} -b ${jail::basejail_location} -l ${jail_location}",
-    creates => $jail_location
+  $file_ensure = $ensure ? {
+    running => present,
+    present => present,
+    stopped => present,
+    absent  => absent
+  }
+  $service_ensure = $ensure ? {
+    running => running,
+    present => stopped,
+    stopped => stopped,
+    absent  => stopped
+
+  }
+
   # Check if the kernel has VIMAGE enabled if the jail has it's own vnet
   # network stack
   if $vnet and !$vimage_enabled {
     fail("vnet for jail ${name} enabled. But no VIMAGE support in Kernel")
   }
+  # Run the script to create the jail
+  unless $ensure == absent {
+    exec {"create-${name}-jail":
+      command => "${jail::create_jail_script} -b ${jail::basejail_location} -l ${jail_location}",
+      creates => $jail_location
+    }
+    Exec["create-${name}-jail"] -> Service[$service_name]
   }
 
+  # Make sure the config file exists
   file { $config_file:
-    ensure  => present,
+    ensure  => $file_ensure,
     path    => $manage_file_path,
     owner   => $jail::config_file_owner,
     group   => $jail::config_file_group,
@@ -94,13 +82,13 @@ define jail::jail(
   }
 
   service { $service_name:
-    ensure     => running,
-    hasrestart => false,
+    ensure     => $service_ensure,
+    hasrestart => true,
     start      => "/usr/sbin/jail -f ${manage_file_path} -c ${name}",
     stop       => "/usr/sbin/jail -f ${manage_file_path} -r ${name}",
-    restart    => "/usr/sbin/jail -f ${manage_file_path} -mr ${name}",
+    restart    => "/usr/sbin/jail -f ${manage_file_path} -rc ${name}",
     status     => "/usr/sbin/jls -j ${name}",
-    require    => [File["jail.conf-${name}"], Exec["create-${name}-jail"]],
+    require    => [File["jail.conf-${name}"]],
   }
 
 }
