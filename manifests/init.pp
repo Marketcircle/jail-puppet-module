fb494801a27d7f00303d0016ebba23d08d4ea459
Thomas Bartelmess <tbartelmess@marketcircle.com>
2014-12-27 13:45:42 -0500

    Removed documentation stubs
    Signed-off-by: Thomas Bartelmess <tbartelmess@marketcircle.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,3 @@
-# == Class: jail
-#
-# Full description of class jail here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { jail:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-
-# === Copyright
-#
-# Copyright 2014 Your name here, unless otherwise noted.
-#
 class jail (
   $config_dir = $jail::params::config_dir,  $scripts_dir = $jail::params::scripts_dir,
   $jails_location = $jail::params::jails_location,
@@ -50,7 +12,7 @@ class jail (
   file { $freebsd_download_path:
     ensure => directory,
   }
-  
+
   file { $puppet_components_download_path:
   	ensure => directory,
   }
