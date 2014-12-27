04f238cd92f730acffcf11f968952a3a85a69733
Thomas Bartelmess <tbartelmess@marketcircle.com>
2014-12-27 14:18:19 -0500

    Travis-CI testing
    Signed-off-by: Thomas Bartelmess <tbartelmess@marketcircle.com>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,27 @@
 require 'spec_helper'
-describe 'jail' do
+describe 'jail', :type => :class do
+
+  let :facts do
+   {:osfamily => 'FreeBSD', :hardwaremodel => 'amd64' }
+ end
 
   context 'with defaults for all parameters' do
-    it { should contain_class('jail') }
-    it { should contain_file('/usr/local/puppet-jail-scripts/') }
+    it { should contain_file('jail.d').with(
+      :ensure => 'directory'
+    )}
+    it { should contain_file('/usr/local/puppet-components').with(
+      :ensure => 'directory'
+    )}
+    it { should contain_file('/usr/local/puppet-jail-scripts').with(
+      :ensure => 'directory'
+    )}
+    it { should contain_file('/usr/local/puppet-jail-scripts/build_jail.sh').with ({
+      :ensure => 'present',
+      :mode => '0744',
+      :owner => 'root',
+      :group => 'wheel'
+    })}
+
+
   end
 end
