69281be69dc65d6cd96b6cc7c6104bdf409fae4a
Thomas Bartelmess <tbartelmess@marketcircle.com>
2014-12-31 23:13:24 -0500

    Removed scripts
    Signed-off-by: Thomas Bartelmess <tbartelmess@marketcircle.com>

--- files/puppet_install.sh
+++
@@ -1,9 +0,0 @@
-PORTS_DIR=/usr/ports
-BASE_JAIL_INSTALL_LOCATION=/usr/local/jails/basejail/usr/
-
-echo "Updating ports tree (Assuming you have ran portsnap extract already"
-portsnap fetch update
-
-echo "Installing puppet"
-cd $PORTS_DIR/sysutils/puppet
-make DESTDIR=$BASE_JAIL_INSTALL_LOCATION PREFIX=/usr install
